15984adeb2007f740f1d1dacabccf261aa84eb53
CojocaruLiviu <new.beta@mail.ru>
2023-07-21 11:39:50 +0300

    asd

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.get('/hello', (request, response) => {
 
 app.use(express.json())
 
-app.use(cors());
 
 app.use('/', authRouter)
 
@@ -31,6 +30,7 @@ app.use((req, res, next) => {
 const start = async () => {
   try{
     await mongoose.connect(MONGODB_URI)
+    app.use(cors());
     app.listen(PORT, ()=>console.log(`server work on port ${PORT}`))
   }catch(e){
     console.log(e)
