87b21e95383652fec837a39d08b87910337214f8
CojocaruLiviu <new.beta@mail.ru>
2023-07-21 11:33:36 +0300

    try

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ app.get('/hello', (request, response) => {
 
 app.use(express.json())
 
+app.use(cors());
+
 app.use('/', authRouter)
 
 app.use(express.static(path.join(__dirname, 'GimnaziulCasunca\FrontGimnaziulCasunca\build')));
@@ -25,7 +27,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'GimnaziulCasunca\FrontGimnaziulCasunca\build', 'index.html'));
 });
 
-app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://band-fuyo.onrender.com/');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
