c5ac68679bbeae77f6ef9b1875f0c274071abc09
CojocaruLiviu <new.beta@mail.ru>
2023-07-21 11:49:06 +0300

    ad

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ app.get('/hello', (request, response) => {
 
 app.use(express.json())
 
-app.use(cors(), authRouter)
+app.use(cors());
+
+app.use('/', authRouter)
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://band-fuyo.onrender.com/');
