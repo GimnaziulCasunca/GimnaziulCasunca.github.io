630695e4eb5e64667f8c4ef8b55f3d892e8213a0
CojocaruLiviu <new.beta@mail.ru>
2023-07-22 20:34:38 +0300

    asddsa

--- index.js
+++ index.js
@@ -22,10 +22,8 @@ app.use(cors());
 
 app.use('/', authRouter)
 
-
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://band-fuyo.onrender.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://gimnaziucasunca.web.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
