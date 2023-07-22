c1d671e6d7ab59e7d678d0db723c49729f64670e
CojocaruLiviu <new.beta@mail.ru>
2023-07-22 19:00:12 +0300

    asd

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors({ origin: 'https://gimnaziucasunca.web.app' }));
+
 const start = async () => {
   try{
     await mongoose.connect(MONGODB_URI)
